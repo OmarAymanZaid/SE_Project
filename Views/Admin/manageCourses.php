5021c5a9656aff1bd3b6eca64229c2af55e54605
OmarAyman <omaraymanzaid1122@gmail.com>
2025-04-17 21:02:43 +0200

    Addition of manageCourses file, changing the logo again

--- Views/Admin/manageCourses.php
+++ Views/Admin/manageCourses.php
@@ -1,56 +1,3 @@
-<?php
-
-    require_once '../../Models/admin.php';
-    require_once '../../Controllers/SessionController.php';
-    require_once '../../Controllers/ConstantsController.php';
-
-    if(!isSessionStarted())
-    {
-        session_start();
-    }
-
-
-    $admin = new Admin;
-    $users = $admin->viewAllUsers();
-
-    $deleteMsg = '';
-    if (isset($_POST["delete"])) 
-    {
-        if (!empty($_POST["userID"])) 
-        {
-          if ($admin->deleteUser($_POST["userID"])) 
-          {
-            $deleteMsg = true;
-            $users = $admin->viewAllUsers();
-          }
-        }
-    }
-
-    $editRoleMsg = '';
-    if (isset($_POST["editRole"])) 
-    {
-        if (!empty($_POST["userID"])) 
-        {
-
-          $role;
-          if($_POST["role"] == 'admin')
-            $role = ADMIN_ROLE;
-          elseif($_POST["role"] == 'student')
-            $role = STUDENT_ROLE;
-          else
-            $role = TEACHER_ROLE;
-
-          if ($admin->editUserRole($_POST["userID"], $role))
-          {
-            $editRoleMsg = true;
-            $users = $admin->viewAllUsers();
-          }
-
-        }
-    }
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -77,10 +24,9 @@
     <link rel="stylesheet" href="../assets/css/ourStyle.css" >
 
 </head>
-
 <body>
 
-    <nav class="pc-sidebar">
+<nav class="pc-sidebar">
         <div class="navbar-wrapper">
 
             <div class="m-header">
@@ -335,111 +281,6 @@
         </div>
     </header>
 
-    <div class="pc-container">
-
-        <div class="card-body">
-            <h3 style="padding:10px;">Users</h3>
-            <div class="dt-responsive table-responsive">
-                <table id="multi-colum-dt" class="table table-striped table-bordered nowrap" style="text-align:center;">
-                <thead>
-                    <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    
-                        foreach($users as $user)
-                        {
-                        ?>
-                            <tr>
-                                <td><?php echo $user['ID']; ?></td>
-                                <td><?php echo $user['name']; ?></td>
-                                <td><?php echo $user['email']; ?></td>
-                                <td>
-                                    <?php
-                                        if($user['roleID'] == ADMIN_ROLE)
-                                            echo "Admin";
-                                        elseif($user['roleID'] == STUDENT_ROLE)
-                                            echo "Student";
-                                        else
-                                            echo "Teacher";
-                                     ?>
-                                </td>
-                                <td>
-                                    <form action="index.php" method="post">
-                                        <input type="hidden" name="userID" value="<?php echo $user["ID"] ?>">
-                                        <button type="submit" class="btn btn-outline-danger" name="delete"><i class='fas fa-trash-alt' style="margin-right: 6px;"></i>Delete</button>
-                                    </form>                   
-                                </td>
-                                <td>
-
-                                    <button type="button" class="btn btn-outline-primary" onclick="openEditForm(<?=$user['ID'];?>)">
-                                        <i class="fas fa-edit"></i>Edit
-                                    </button>
-
-                                    <div class="justify-content-center container" style="border: 1px gray solid; margin-top:15px; padding: 15px; display:none;" id="editForm-<?=$user['ID'];?>">
-                                        <form action="index.php" method="post">
-                                            <h2>Edit User Role</h2>
-                                            <div class="container">
-                                                <div>
-                                                    <label class="form-label">Admin</label>
-                                                    <input type="radio" name="role" value="admin">
-                                                </div>
-                                                <div>
-                                                    <label class="form-label">Student</label>
-                                                    <input type="radio" name="role" value="student">
-                                                </div>
-                                                <div>
-                                                    <label class="form-label">Teacher</label>
-                                                    <input type="radio" name="role" value="teacher">
-                                                </div>
-                                            </div>
-                                            <input type="hidden" value = "<?=$user['ID'];?>" name="userID">
-                                            <button type="submit" class="btn btn-outline-danger" name="editRole">
-                                                Edit
-                                            </button>
-                                        </form>                   
-                                    </div>
-                                </td>
-                            </tr>
-                        <?php
-                        }
-                    
-                    ?>
-                </tbody>
-                </table>
-
-                <?php
-                    if($deleteMsg)
-                    {
-                        ?>
-                        <div class="alert alert-success" role="alert"> 
-                            <?php echo 'User deleted successfully' ; ?>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </div>
-                        <?php
-                    }
-                ?>
-
-            </div>
-        </div>
-
-        <div style="padding:10px;">
-            <a class="btn btn-primary" href="addUser.php" role="button">Add New User</a>
-         </div>
-
-    </div>
-
-    <script>        
-        function openEditForm(userID)
-        {
-            document.getElementById('editForm-' + userID).style.display = "flex";
-        }
-    </script>
     <!-- [Page Specific JS] start -->
     <script src="../assets/js/plugins/apexcharts.min.js"></script>
     <script src="../assets/js/pages/dashboard-default.js"></script>
