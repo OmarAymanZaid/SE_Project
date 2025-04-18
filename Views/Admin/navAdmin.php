19fcda8871d185990930f3ffe0d20e8c3a0a604b
OmarAyman <omaraymanzaid1122@gmail.com>
2025-04-18 10:40:57 +0200

    adding new files for reusablitiy

---
+++ Views/Admin/navAdmin.php
@@ -0,0 +1,35 @@
+<nav class="pc-sidebar">
+        <div class="navbar-wrapper">
+
+            <div class="m-header">
+                <a href="../dashboard/index.html" class="b-brand text-primary">
+                    <!-- ========   Change your logo from here   ============ -->
+                    <img src="../assets/images/edusmarto-logo2.png" class="img-fluid" alt="logo" style="margin-top:30px;">
+                </a>
+            </div>
+
+            <div class="navbar-content">
+                <ul class="pc-navbar">
+
+                    <li class="pc-item"> </li>
+                    <li class="pc-item pc-caption">
+                        <label>Pages</label>
+                        <i class="ti ti-news"></i>
+                    </li>
+
+                    <li class="pc-item">
+                        <a href="index.php" class="pc-link">
+                            <span class="pc-micon"><i class="ti ti-user-plus"></i></span>
+                            <span class="pc-mtext">Manage Users</span>
+                        </a>
+                    </li>
+                    <li class="pc-item">
+                        <a href="manageCourses.php" class="pc-link">
+                            <span class="pc-micon"><i class="ti ti-book"></i></span>
+                            <span class="pc-mtext">Manage Courses</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+</nav>
