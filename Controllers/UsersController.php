959f810841dcdd01c0b0720e4181e033a1716143
OmarAyman <omaraymanzaid1122@gmail.com>
2025-04-29 20:29:35 +0300

    some stuff in the admin module

--- Controllers/UsersController.php
+++ Controllers/UsersController.php
@@ -25,6 +25,25 @@ class UsersController
         }
     }
 
+    public function getAllTeachersExcept($userID)
+    {
+        $this->db = new DBController;
+
+        if($this->db->openConnection())
+        {
+            $qry    = "SELECT ID, name, email, roleID, image FROM users WHERE roleID = " . TEACHER_ROLE . " and ID != $userID;";
+            $result = $this->db->select($qry);
+
+            $this->db->closeConnection();
+            return $result;
+        }
+        else
+        {
+            echo "Error in database connection";
+            return false;
+        }
+    }
+
 }
 
 ?>
---
+++ Views/Teacher/navTeacher.php
@@ -0,0 +1,46 @@
+<nav class="pc-sidebar">
+        <div class="navbar-wrapper">
+
+            <div class="m-header">
+                <a href="index.php" class="b-brand text-primary">
+                    <!-- ========   Change your logo from here   ============ -->
+                    <img src="../assets/images/edusmarto-logo2.png" class="img-fluid" alt="logo" style="margin-top:30px;">
+                </a>
+            </div>
+
+            <div class="navbar-content">
+                <ul class="pc-navbar">
+
+                    <li class="pc-item"> </li>
+                    <li class="pc-item pc-caption">
+                        <label>Pages</label>
+                        <i class="ti ti-news"></i>
+                    </li>
+
+                    <li class="pc-item">
+                        <a href="index.php" class="pc-link">
+                            <span class="pc-micon"><i class="ti ti-book"></i></span>
+                            <span class="pc-mtext">Courses</span>
+                        </a>
+                    </li>
+
+                    <li class="pc-item">
+                        <a href="evaluatePeers.php" class="pc-link">
+                            <span class="pc-micon"><i class="ti ti-stars"></i></span>
+                            <span class="pc-mtext">Ratings</span>
+                        </a>
+                    </li>
+
+
+                    <!-- <li class="pc-item">
+                        <a href="assignments.php" class="pc-link">
+                            <span class="pc-micon"><i class="ti ti-clipboard-check"></i></span>
+                            <span class="pc-mtext">Assignments</span>
+                        </a>
+                    </li> -->
+
+
+                </ul>
+            </div>
+        </div>
+</nav>
