4002b01529fdcba1c4c44c6c9cb79ff3f9bd3f74
OmarAyman <omaraymanzaid1122@gmail.com>
2025-05-08 16:51:17 +0300

    Fixing some MVC stuff

--- Controllers/AuthController.php
+++ Controllers/AuthController.php
@@ -18,13 +18,65 @@ class AuthController
     public function login(User $user)
     {
         $userModel = new User();
-        return $userModel->login($user);
+        $result = $userModel->login($user);
+
+        if($result === false)
+        {
+            echo 'error in query';
+            return false ;
+        }
+        else
+        {
+            if(count($result) == 0)
+            {
+                $_SESSION['errMsg'] = 'wrong email or password';
+                return false ;
+            }
+            else
+            {
+                $_SESSION['userID'] = $result[0]['ID'];
+                $_SESSION['userName'] = $result[0]['name'];
+                $_SESSION['userEmail'] = $result[0]['email'];
+                $_SESSION['userImage'] = $result[0]['image'];
+                
+                if($result[0]['roleID'] == ADMIN_ROLE)
+                {
+                    $_SESSION['userRole'] = 'admin';
+                }
+                else if($result[0]['roleID'] == STUDENT_ROLE)
+                {
+                    $_SESSION['userRole'] = 'student';
+                }
+                else
+                {
+                    $_SESSION['userRole'] = 'teacher';
+                }
+
+                return true;
+            }
+        }
     }
 
     public function register(User $user)
     {
         $userModel = new User();
-        return $userModel->register($user);
+        $result    = $userModel->register($user);
+
+        if($result != false)
+        {
+            $_SESSION['userID'] = $result;
+            $_SESSION['userName'] = $user->name;
+            $_SESSION['userImage'] = $user->image;
+            if($user->roleID == ADMIN_ROLE)
+                $_SESSION['userRole'] = "admin";
+            elseif($user->roleID == STUDENT_ROLE)
+                $_SESSION['userRole'] = "student";
+            else
+                $_SESSION['userRole'] = "teacher";
+
+            return true;
+        }
+
     }
 
     public function logout()
--- Models/user.php
+++ Models/user.php
@@ -20,41 +20,7 @@ class User
         $qry = "select * from users where email = '$email' and password='$password'";
         $result = $this->db->select($qry);
 
-        if($result === false)
-        {
-            echo 'error in query';
-            return false ;
-        }
-        else
-        {
-            if(count($result) == 0)
-            {
-                $_SESSION['errMsg'] = 'wrong email or password';
-                return false ;
-            }
-            else
-            {
-                $_SESSION['userID'] = $result[0]['ID'];
-                $_SESSION['userName'] = $result[0]['name'];
-                $_SESSION['userEmail'] = $result[0]['email'];
-                $_SESSION['userImage'] = $result[0]['image'];
-                
-                if($result[0]['roleID'] == ADMIN_ROLE)
-                {
-                    $_SESSION['userRole'] = 'admin';
-                }
-                else if($result[0]['roleID'] == STUDENT_ROLE)
-                {
-                    $_SESSION['userRole'] = 'student';
-                }
-                else
-                {
-                    $_SESSION['userRole'] = 'teacher';
-                }
-
-                return true;
-            }
-        }
+        return $result;
 
     }
 
@@ -79,22 +45,7 @@ class User
         $qry = "insert into users values('' , '$name' , '$email' , '$password' ,'$role','$image')";
         $result = $this->db->insert($qry);
 
-        if($result != false)
-        {
-            $_SESSION['userID'] = $result;
-            $_SESSION['userName'] = $user->name;
-            $_SESSION['userImage'] = $user->image;
-            if($user->roleID == ADMIN_ROLE)
-                $_SESSION['userRole'] = "admin";
-            elseif($user->roleID == STUDENT_ROLE)
-                $_SESSION['userRole'] = "student";
-            else
-                $_SESSION['userRole'] = "teacher";
-
-            return true;
-        }
-        
-
+        return $result;        
     }
 
     public function logout()
